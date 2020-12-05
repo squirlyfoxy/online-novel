7d5e44f8188627a3dce890866ebc943fde7f7f25
Tommaso Brandinelli <48562227+MayonaiseMan@users.noreply.github.com>
2020-12-05 17:24:15 +0100

    first step creazione log in

---
+++ src/usr/index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="stile.css">
+
+    <title>Online Novels</title>
+  </head>
+  <body>
+
+
+    <header>
+
+    </header>
+
+    <h1>Log in</h1>
+
+    <div>
+      <div class="container" id="input_dati_registration" align="center">
+        <form>
+          <div class="form-group">
+              <label for="InputEmail1">Email</label>
+              <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp">
+          </div>
+          <div class="form-group">
+              <label for="InputPassword1">Password</label>
+              <input type="password" class="form-control" id="InputPassword1">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+
+    <footer class="container" >
+           <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-bottom ">
+             <div class="collapse navbar-collapse" id="navbarSupportedContentFooter">
+             <ul class="navbar-nav mr-auto">
+             <li class="nav-item">
+               <a class="nav-link" href="about_us.html">membri</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="donation.html" tabindex="-1" aria-disabled="true">contatti</a>
+             </li>
+             </ul>
+
+             </div>
+           </nav>
+     </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+  </body>
+</html>
