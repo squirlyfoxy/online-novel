5283dd0c59349f6cfd219bf231fdde598554d4ec
squirlyfoxy <public.thesquirlyz97@gmail.com>
2020-12-06 12:23:03 +0100

    Scheletro dell'index

--- src/novels/index.php
+++ src/novels/index.php
@@ -42,23 +42,22 @@
         <title>Online Novels - Romanzi</title>
     </head>
     <body>
-        <body>
-            <!--- MENU !--->
-            <nav class="navbar navbar-inverse navbar-expand-lg navbar-dark bg-dark fixed-top">
-                <a class="navbar-brand" href="../index.php">Online Novels</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="">Archivio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Classifiche</a>
-                        </li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
+        <!--- MENU !--->
+        <nav class="navbar navbar-inverse navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <a class="navbar-brand" href="../index.php">Online Novels</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Archivio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Classifiche</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
                     <?php
                         echo '<li class="nav-item" >';
 
@@ -75,15 +74,19 @@
                         echo '<img id="right" src="'.$user_icon.'" width="32" />';
                         echo '</li>';
                     ?>
-                    </ul>   
-                    <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Ricerca" aria-label="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit" onclick="">
-                            <img src="../img/src.png" width="32" />
-                        </button>
-                    </form>
-                </div>
-            </nav>
+                </ul>   
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Ricerca" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" onclick="">
+                        <img src="../img/src.png" width="32" />
+                    </button>
+                </form>
+            </div>
+        </nav>
+
+        <h2>Serie Popolari</h2>
+
+        <h2>Ultime Aggiunte</h2>
 
         <!--- SCRIPTS !--->
 
