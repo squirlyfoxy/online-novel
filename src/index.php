e3839a97426959b2a4e858cd8d544ae961ba9745
Tommaso Brandinelli <48562227+MayonaiseMan@users.noreply.github.com>
2020-12-06 02:24:00 +0100

    third step, definizione quasi completa di log-in e registrazion, inizio scheletro per Home

---
+++ src/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+          <a class="navbar-brand" href="index.html" >Home</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="">Archivio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Classifiche</a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit" onclick="">Search</button>
+            </form>
+          </div>
+        </nav>
+      </div>
+    </header>
+
+    <h2>Serie Popolari</h2>
+
+    <div class="">
+
+    </div>
+
+    <footer class="container" >
+           <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-bottom ">
+             <div class="collapse navbar-collapse" id="navbarSupportedContentFooter">
+             <ul class="navbar-nav mr-auto">
+             <li class="nav-item">
+               <a class="nav-link" href="about_us.html">membri</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="donation.html" tabindex="-1" aria-disabled="true">contatti</a>
+             </li>
+             </ul>
+
+             </div>
+           </nav>
+     </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+  </body>
+</html>
