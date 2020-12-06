63839117b7ad491606de02938cf8c9000b447c5e
squirlyfoxy <public.thesquirlyz97@gmail.com>
2020-12-06 12:08:11 +0100

    aggiunta del menu (preso in prestito dal codice di brando)

--- src/novels/index.php
+++ src/novels/index.php
@@ -1,5 +1,29 @@
 <?php
     session_start();
+
+    ///
+    /// Codice per controllare se siamo loggati oppure no
+    /// Codice per avere l'icona dell'utente
+    ///
+
+    $usr_name = "";
+    $usr_id = 0;
+    $user_icon = "../img/usr.png";
+    $is_logged;
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        //PER POST RICEVO LE INFORMAZIONI DELL'UTENTE
+
+        //Controlla se sono giuste confrontandole con il database
+    }
+
+    //Se non ricevo nulla vuol dire che non sono loggato, quindi:
+    if($usr_name == "" && $usr_name == "")
+    {
+        $is_logged = false;
+    }
+
 ?>
 
 <html lang="it">
@@ -10,9 +34,56 @@
         <!--- BOOTSTRAP !--->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
+        <!--- CUSTOM CSS !--->
+        <link rel="stylesheet" href="../css/nav-bar.css">
+
         <title>Online Novels - Romanzi</title>
     </head>
     <body>
+        <body>
+            <!--- MENU !--->
+            <nav class="navbar navbar-inverse navbar-expand-lg navbar-dark bg-dark fixed-top">
+                <a class="navbar-brand" href="../index.php">Online Novels</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Archivio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Classifiche</a>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                    <?php
+                        echo '<li class="nav-item" >';
+
+                        if($is_logged == false) //Non sono loggato, dai la possibilità di farlo
+                        {
+                            echo '
+                                <a class="nav-link" id="right" href="../usr">Login/Registrati</a>';
+                        } else
+                        {
+                            //Informazioni dell'utente loggato
+                            echo '
+                                <a class="nav-link" id="right" href="../usr/about/">'.$usr_name.'</a>';
+                        }
+                        echo '<img src="'.$user_icon.'" width="32"  style="margin-left: 5px; margin-right: 10px;"/>';
+                        echo '</li>';
+                    ?>
+                    </ul>   
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Ricerca" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit" onclick="">
+                            <img src="../img/src.png" width="32" />
+                        </button>
+                    </form>
+                </div>
+            </nav>
+
+        <!--- SCRIPTS !--->
 
         <!--- BOOTSTRAP !--->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
--- src/novels/visualizer/index.php
+++ src/novels/visualizer/index.php
@@ -6,8 +6,9 @@
     /// l'ultima pagina.
     ///
 
-    // VAriabili
+    // Variabili
     $novel_id = "";
+    $novel_name = "";
     $position_frame = "";
 
     /// Per get io avrò la posizione e l'id del romanzo. Se non ricevo nulla visualizzo alcuni romanzi a video
@@ -33,6 +34,33 @@
         // Redirect a ../
         header("location: ../");
     }
+
+    //TODO: Connettersi al database per avere il nome del romanzo e viasualizzarlo a video
+    
+    //TODO: Connettersi al database per avere il contenuto della pagina e visualizzarla
+
+    ///
+    /// Codice per controllare se siamo loggati oppure no
+    /// Codice per avere l'icona dell'utente
+    ///
+
+    $usr_name = "";
+    $usr_id = 0;
+    $user_icon = "../../img/usr.png";
+    $is_logged;
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        //PER POST RICEVO LE INFORMAZIONI DELL'UTENTE
+
+        //Controlla se sono giuste confrontandole con il database
+    }
+
+    //Se non ricevo nulla vuol dire che non sono loggato, quindi:
+    if($usr_name == "" && $usr_name == "")
+    {
+        $is_logged = false;
+    }
 ?>
 
 <html lang="it">
@@ -43,13 +71,64 @@
         <!--- BOOTSTRAP !--->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
+        <!--- CUSTOM CSS !--->
+        <link rel="stylesheet" href="../../css/nav-bar.css">
+
         <title>Online Novels - 
         <?php
             //Codice per visualizzare il nome del romanzo
+            echo $novel_name;
         ?>
             </title>
     </head>
     <body>
+        <!--- MENU !--->
+        <nav class="navbar navbar-inverse navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <a class="navbar-brand" href="../index.php">Online Novels</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Archivio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="" tabindex="-1" aria-disabled="true">Classifiche</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                <?php
+                    echo '<li class="nav-item" >';
+
+                    if($is_logged == false) //Non sono loggato, dai la possibilità di farlo
+                    {
+                        echo '
+                            <a class="nav-link" id="right" href="../usr">Login/Registrati</a>';
+                    } else
+                    {
+                        //Informazioni dell'utente loggato
+                        echo '
+                            <a class="nav-link" id="right" href="../usr/about/">'.$usr_name.'</a>';
+                    }
+                    echo '<img src="'.$user_icon.'" width="32"  style="margin-left: 5px; margin-right: 10px;"/>';
+                    echo '</li>';
+                ?>
+                </ul>   
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Ricerca" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" onclick="">
+                        <img src="../img/src.png" width="32" />
+                    </button>
+                </form>
+            </div>
+        </nav>
+
+        <?php
+            //Visualizzare la pagina del romanzo
+        ?>
+
+        <!--- SCRIPTS !--->
 
         <!--- BOOTSTRAP !--->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
